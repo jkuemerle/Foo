ec4e6f189b94e4425ef74ff014e1542817deea35
Joe Kuemerle <joe@kuemerle.com>
2013-09-03 12:18:34 -0400

    Initial source

---
+++ MvcWebRole1/Models/Home.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebRole1.Models
+{
+    public class Home
+    {
+        public string Version { get; set; }
+        public string WebConfig { get; set; }
+        public string WebRoleConfig { get; set; }
+
+        public Home()
+        {
+            this.WebConfig = System.Configuration.ConfigurationManager.AppSettings["Setting"];
+            this.Version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(4);
+            this.WebRoleConfig = Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment.GetConfigurationSettingValue("Environment");
+        }
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+JoeSoft Azure Cloud Service application.
