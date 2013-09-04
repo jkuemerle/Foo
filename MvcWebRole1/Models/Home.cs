dc2bfc299243501e2fc9a6fcb10ef69ceee94e35
Joe Kuemerle <joe@kuemerle.com>
2013-09-04 18:51:01 -0400

    Added Azure Web Site App

--- MvcWebRole1/Models/Home.cs
+++ MvcWebRole1/Models/Home.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace MvcWebRole1.Models
+namespace Models
 {
     public class Home
     {
         public string Version { get; set; }
         public string WebConfig { get; set; }
         public string WebRoleConfig { get; set; }
-
         public string Build { get; set; }
+      
         public Home()
         {
             this.WebConfig = System.Configuration.ConfigurationManager.AppSettings["Setting"];
             this.Build = System.Configuration.ConfigurationManager.AppSettings["Build"];
             this.Version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString(4);
+#if AZURE
             this.WebRoleConfig = Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment.GetConfigurationSettingValue("Environment");
+#endif
         }
 
     }
